722f32766126365e3532ebbd8a803bc1a8479760
Gustavo <gustavomarianosoares@gmail.com>
2021-09-08 20:49:31 -0300

    Projeto criado
    Projeto criado, com css separado e já com os icones e splash screens colocados

---
+++ assets/css/css.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet} from 'react-native'
+
+const css = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export {css};
